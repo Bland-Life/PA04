36f9e9ef57e9abbd13aa71d50ed06c5d2e3348e1
Donaldo Martinez <do073496@ucf.edu>
2024-04-02 15:48:53 -0400

    Update pa04.c

--- pa04.c
+++ pa04.c
@@ -34,11 +34,11 @@ customer* create_customer(char* name, int points);
 void add_tree_node();
 void delete_tree_node();
 void subtract_customer_points();
-int search_customer_tree();
+node* search_tree();
 
 
 int main(int argc, char* argv[]) {
-
+    
     FILE* input_fp;
     if ((input_fp = fopen(argv[1], "r")) == NULL) {
         return -1;
@@ -63,7 +63,10 @@ int main(int argc, char* argv[]) {
 
         fgets(buffer, MAX_LINE_LENGTH, input_fp);
         char command[13];
-        char name[19];
+        char* name = (char*) malloc(sizeof(char) * NAME_LENGTH);
+        if (name == NULL) {
+            return -1;
+        }
         int points;
         sscanf(buffer, "%s %s %d", command, name, &points);
         
@@ -73,6 +76,8 @@ int main(int argc, char* argv[]) {
             if (person == NULL) {
                 return -1;
             }
+
+            add_tree_node(root, person);
         }
         else if (strcmp(command, "sub") == 0) {
             printf("SUB\n");
@@ -101,6 +106,121 @@ customer* create_customer(char* name, int points) {
     return person;
 }
 
-void add_tree_node() {
+node* init_node(customer* person) {
+    node* temp = (node*) malloc(sizeof(node));
+    if (temp == NULL) {
+        printf("Error allocating memory for new tree node");
+        return temp;
+    }
+
+    temp->person = person;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+node* search_tree(node* root, char* name) {
+    if (root->person == NULL) {
+        return root;
+    }
+
+    if (strcmp(name, root->person->name) == 0) {
+        printf("%s %d %d", root->person->name, root->person->points, root->person->node_depth);
+        return root;
+    }
+    else if (strcmp(name, root->person->name) < 0) {
+        if (root->left == NULL) {
+            return root;
+        }
+        else if (strcmp(name, root->left->person->name) > 0) {
+            return root;
+        }
+        else {
+            search_tree(root->left, name);
+        }
+    }
+    else if (strcmp(name, root->person->name) > 0) {
+        if (root->right == NULL) {
+            return root;
+        }
+        else if (strcmp(name, root->right->person->name) < 0) {
+            return root;
+        }
+        else {
+            search_tree(root->right, name);
+        }
+    }
+
+}
+
+void add_tree_node(node* root, customer* person) {
     
+    // If the person field is empty then the root node is as well, so add the person to the root node 
+    // and leave the children null
+    if (root->person == NULL) {
+        root->person = person;
+        return;
+    }
+
+    // If the person's name comes (alphabetically) before the current node
+    if (strcmp(person->name, root->person->name) < 0) {
+
+        // If the current node doesn't have a left child, create a new node with the person and make it
+        // the left child of the current node
+        if (root->left == NULL) {
+            node* new_node = init_node(person);
+            root->left = new_node;
+            printf("Left Leaf: %s\n", new_node->person->name);
+            return;
+        }
+
+        // Else if the current node has a left child and the left child has a name that comes before the person
+        // being added, make a new node and place it between the root node and left child node
+        else if (strcmp(person->name, root->left->person->name) > 0) {
+            node* new_node = init_node(person);
+            new_node->left = root->left;
+            root->left = new_node;
+            printf("Left Between: %s\n", new_node->person->name);
+            return;
+        }
+
+        // Finally, if none of the other conditions are true then the current person must continue to traverse down
+        // the left of the tree
+        else {
+            add_tree_node(root->left, person);
+        }
+    }
+
+    // If the person's name comes (alphabetically) after the current node
+    else if (strcmp(person->name, root->person->name) > 0) {
+        // printf("%s    %s   %d", person->name, root->person->name, strcmp(person->name, root->person->name));
+        // If the current node doesn't have a right child, create a new node with the person and make it
+        // the right child of the current node
+        if (root->right == NULL) {
+            node* new_node = init_node(person);
+            root->right = new_node;
+            printf("Right Leaf: %s\n", new_node->person->name);
+            return;
+        }
+
+        // Else if the current node has a right child and the right child has a name that comes after the person
+        // being added, make a new node and place it between the root node and right child node
+        else if (strcmp(person->name, root->right->person->name) < 0) {
+            node* new_node = init_node(person);
+            new_node->right = root->right;
+            root->right = new_node;
+            printf("Right Between: %s\n", new_node->person->name);
+            return;
+        }
+
+        // Finally, if none of the other conditions are true then the current person must continue to traverse down
+        // the right of the tree
+        else {
+            add_tree_node(root->right, person);
+        }
+    }
+
+    else if (strcmp(person->name, root->person->name) == 0) {
+        root->person->points += person->points;
+    }
 }
